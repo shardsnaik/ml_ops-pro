7a2189b0e78bf2c182bee980ebf7c0993b70af21
shardsnaik <sharadsanik001@gmail.com>
2024-11-01 22:59:55 +0530

    updating first pipeline test

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ from src.CnnClasifier.pipeline.stage_03_model_training import model_training
 from src.CnnClasifier import logger
 
 
-# if __name__ == '__main__':
-# stage_name = 'Data Ingestion Stage'
-#     try:
-#         logger.info('f>>>>>>>>> stage {stage_name} started <<<<<<<<')
-#         data_ingestion = Data_imgestionTraningPipeline()
-#         data_ingestion.main()
-#         logger.info(f'>>>>>>>>>> sttage {stage_name} completed... <<<<<<<<<<<<<<')    
-#     except Exception as e:
-#         logger.exception(e)
-#         raise e
+if __name__ == '__main__':
+    stage_name = 'Data Ingestion Stage'
+    try:
+        logger.info('f>>>>>>>>> stage {stage_name} started <<<<<<<<')
+        data_ingestion = Data_imgestionTraningPipeline()
+        data_ingestion.main()
+        logger.info(f'>>>>>>>>>> sttage {stage_name} completed... <<<<<<<<<<<<<<')    
+    except Exception as e:
+        logger.exception(e)
+        raise e
     
 
 # if __name__ =='__main__':
@@ -28,13 +28,13 @@ from src.CnnClasifier import logger
     
 
 
-if __name__ == '__main__':
-     stage_name = 'Model training'
-     try:
-        logger.info('f>>>>>>>>>>> stage of  >>>>>>>>')
-        logger.info('f>>>>>>>>>>> MODEL TRAINING STARTED >>>>>>>>')
-        tra = model_training()
-        tra.main()
-        logger.info('f<<<<<<<<<<<<<<< stage of {stage_name} MODEL TRAINING FINISHED<<<<<<<<<<<<<<<<<<<')
-     except Exception as e:
-       raise e
+# if __name__ == '__main__':
+#      stage_name = 'Model training'
+#      try:
+#         logger.info('f>>>>>>>>>>> stage of  >>>>>>>>')
+#         logger.info('f>>>>>>>>>>> MODEL TRAINING STARTED >>>>>>>>')
+#         tra = model_training()
+#         tra.main()
+#         logger.info('f<<<<<<<<<<<<<<< stage of {stage_name} MODEL TRAINING FINISHED<<<<<<<<<<<<<<<<<<<')
+#      except Exception as e:
+#        raise e
