2b65ae4d7bccb63861c59889b02876f962a4daf6
shardsnaik <sharadsanik001@gmail.com>
2024-11-01 23:01:23 +0530

    updating second pipeline test

--- main.py
+++ main.py
@@ -4,27 +4,27 @@ from src.CnnClasifier.pipeline.stage_03_model_training import model_training
 from src.CnnClasifier import logger
 
 
-if __name__ == '__main__':
-    stage_name = 'Data Ingestion Stage'
-    try:
-        logger.info('f>>>>>>>>> stage {stage_name} started <<<<<<<<')
-        data_ingestion = Data_imgestionTraningPipeline()
-        data_ingestion.main()
-        logger.info(f'>>>>>>>>>> sttage {stage_name} completed... <<<<<<<<<<<<<<')    
-    except Exception as e:
-        logger.exception(e)
-        raise e
-    
-
-# if __name__ =='__main__':
-#     stage_name = 'preparing base model'
+# if __name__ == '__main__':
+#     stage_name = 'Data Ingestion Stage'
 #     try:
 #         logger.info('f>>>>>>>>> stage {stage_name} started <<<<<<<<')
-#         run = pre_bs_mdl()
-#         run.main()
-#         logger.info(f'>>>>>>>>>> sttage {stage_name} completed... <<<<<<<<<<<<<<')
+#         data_ingestion = Data_imgestionTraningPipeline()
+#         data_ingestion.main()
+#         logger.info(f'>>>>>>>>>> sttage {stage_name} completed... <<<<<<<<<<<<<<')    
 #     except Exception as e:
-#        raise e
+#         logger.exception(e)
+#         raise e
+    
+
+if __name__ =='__main__':
+    stage_name = 'preparing base model'
+    try:
+        logger.info('f>>>>>>>>> stage {stage_name} started <<<<<<<<')
+        run = pre_bs_mdl()
+        run.main()
+        logger.info(f'>>>>>>>>>> sttage {stage_name} completed... <<<<<<<<<<<<<<')
+    except Exception as e:
+       raise e
     
 
 
